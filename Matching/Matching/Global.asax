7b031a042aa8ddf82cf0ac7a1f097c9f43eb3846
agnieszkaborusiak <agnieszkaborusiak@gmail.com>
2015-12-06 14:12:42 +0100

    Base Project Version

---
+++ Matching/Matching/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Matching.MvcApplication" Language="C#" %>
---
+++ Matching/Matching/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Matching.Startup))]
+namespace Matching
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
