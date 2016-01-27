2c1397821a2387c57c8e6d1d7dd194957b88e1b1
agnieszkaborusiak <agnieszkaborusiak@gmail.com>
2016-01-27 23:00:10 +0100

    pakiet startowy API

---
+++ Matching2/Matching2/Matching2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Matching2.Startup))]
+namespace Matching2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
